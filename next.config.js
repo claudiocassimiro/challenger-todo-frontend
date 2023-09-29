dc87cb93f329833034b308a685296d9a2f6c9894
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-29 11:57:27 -0300

    feat: add new next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+  // images: {
+  //   domains: ['localhost', 'res.cloudinary.com', 'meu-menu-digital.vercel.app'],
+  // },
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
